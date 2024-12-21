f90b28c7f45adaedcb81f882692e8a2efe32f867
intellign <ismailarashad@aol.com>
2024-12-21 11:25:00 -0500

    Update giphy_grid_view.dart

--- lib/src/widgets/giphy_grid_view.dart
+++ lib/src/widgets/giphy_grid_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:modal_gif_picker/src/model/giphy_repository.dart';
 import 'package:modal_gif_picker/src/widgets/giphy_context.dart';
 import 'package:modal_gif_picker/src/widgets/giphy_image_thumbnail.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 /// A selectable grid view of gif thumbnails.
 class GiphyGridView extends StatelessWidget {
@@ -30,28 +31,50 @@ class GiphyGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        //  addAutomaticKeepAlives: true,
-        padding: const EdgeInsets.all(10),
-        controller: scrollController,
-        itemCount: repo.totalCount,
-        //   shrinkWrap: true,
-        physics: BouncingScrollPhysics(),
-        itemBuilder: (BuildContext context, int index) => GestureDetector(
-            child: GiphyImageThumbnail(
-                useUrlToSaveMemory: useUrlToSaveMemory,
-                key: Key('$index'),
-                repo: repo,
-                index: index),
-            onTap: () async {
-              final giphy = GiphyContext.of(context);
-              final gif = await repo.get(index);
-              giphy.onSelected?.call(gif);
-            }),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    return true
+        ? MasonryGridView.count(
+            addAutomaticKeepAlives: true,
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            physics: BouncingScrollPhysics(),
+            controller: scrollController,
+            itemCount: repo.totalCount,
             crossAxisCount: crossAxisCount,
-            childAspectRatio: childAspectRatio,
             crossAxisSpacing: crossAxisSpacing,
-            mainAxisSpacing: mainAxisSpacing));
+            mainAxisSpacing: mainAxisSpacing,
+            itemBuilder: (BuildContext context, int index) => GestureDetector(
+                child: GiphyImageThumbnail(
+                    useUrlToSaveMemory: useUrlToSaveMemory,
+                    key: Key('$index'),
+                    repo: repo,
+                    index: index),
+                onTap: () async {
+                  final giphy = GiphyContext.of(context);
+                  final gif = await repo.get(index);
+                  giphy.onSelected?.call(gif);
+                }),
+          )
+        : GridView.builder(
+            //  addAutomaticKeepAlives: true,
+            padding: const EdgeInsets.all(10),
+            controller: scrollController,
+            itemCount: repo.totalCount,
+            //   shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (BuildContext context, int index) => GestureDetector(
+                child: GiphyImageThumbnail(
+                    useUrlToSaveMemory: useUrlToSaveMemory,
+                    key: Key('$index'),
+                    repo: repo,
+                    index: index),
+                onTap: () async {
+                  final giphy = GiphyContext.of(context);
+                  final gif = await repo.get(index);
+                  giphy.onSelected?.call(gif);
+                }),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: crossAxisCount,
+                childAspectRatio: childAspectRatio,
+                crossAxisSpacing: crossAxisSpacing,
+                mainAxisSpacing: mainAxisSpacing));
   }
 }
