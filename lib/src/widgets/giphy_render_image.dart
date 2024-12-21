20b9440078df6127393c9bb30fb7d2f1ebc6be91
intellign <ismailarashad@aol.com>
2024-12-21 11:24:12 -0500

    Update giphy_render_image.dart

--- lib/src/widgets/giphy_render_image.dart
+++ lib/src/widgets/giphy_render_image.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:modal_gif_picker/src/model/client/gif.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:extended_image/extended_image.dart';
 
 /// Loads and renders a Giphy image.
 class GiphyRenderImage extends StatefulWidget {
@@ -13,6 +14,7 @@ class GiphyRenderImage extends StatefulWidget {
   final BoxFit? fit;
   final bool renderGiphyOverlay;
   final bool useUrlToSaveMemory;
+  final GiphyGif gif;
 
   /// Loads an image from given url.
   const GiphyRenderImage(
@@ -22,6 +24,7 @@ class GiphyRenderImage extends StatefulWidget {
       this.width,
       this.height,
       this.fit,
+      required this.gif,
       this.renderGiphyOverlay = true,
       this.useUrlToSaveMemory = false})
       : super(key: key);
@@ -29,7 +32,7 @@ class GiphyRenderImage extends StatefulWidget {
   /// Loads the original image for given Giphy gif.
   GiphyRenderImage.original(
       {Key? key,
-      required GiphyGif gif,
+      required this.gif,
       this.placeholder,
       this.width,
       this.height,
@@ -42,7 +45,7 @@ class GiphyRenderImage extends StatefulWidget {
   /// Loads the original still image for given Giphy gif.
   GiphyRenderImage.originalStill(
       {Key? key,
-      required GiphyGif gif,
+      required this.gif,
       this.placeholder,
       this.width,
       this.height,
@@ -55,7 +58,7 @@ class GiphyRenderImage extends StatefulWidget {
   /// Loads the original still image for given Giphy gif.
   GiphyRenderImage.fixedWidth(
       {Key? key,
-      required GiphyGif gif,
+      required this.gif,
       this.placeholder,
       this.width,
       this.height,
@@ -97,33 +100,107 @@ class _GiphyRenderImageState extends State<GiphyRenderImage> {
         const Center(child: CircularProgressIndicator());
   }
 
+  Widget gifWidget() {
+    double _aspectRatio = 0.0;
+    _aspectRatio = (double.parse(widget.gif.images.fixedWidth!.width!) /
+        double.parse(widget.gif.images.fixedWidth!.height!));
+
+    return true
+        ? ExtendedImage.network(
+            widget.gif.images.fixedWidth!.webp!,
+            semanticLabel: widget.gif.title,
+            cache: true,
+            gaplessPlayback: true,
+            fit: BoxFit.fill,
+            headers: const {'accept': 'image/*'},
+            loadStateChanged: (state) => AnimatedSwitcher(
+              duration: const Duration(milliseconds: 350),
+              child: widget.gif.images.fixedWidth == null
+                  ? Container()
+                  : case2(
+                      state.extendedImageLoadState,
+                      {
+                        LoadState.loading: AspectRatio(
+                          aspectRatio: _aspectRatio,
+                          child: Container(
+                            alignment: Alignment.center,
+                            color: Colors.transparent,
+                            height: 30,
+                            width: 30,
+                            child: const CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation(Colors.white54),
+                              strokeWidth: 1,
+                            ),
+                          ),
+                        ),
+                        LoadState.completed: AspectRatio(
+                          aspectRatio: _aspectRatio,
+                          child: ExtendedRawImage(
+                            fit: BoxFit.fill,
+                            image: state.extendedImageInfo?.image,
+                          ),
+                        ),
+                        LoadState.failed: AspectRatio(
+                          aspectRatio: _aspectRatio,
+                          child: Container(
+                            color: Theme.of(context).cardColor,
+                          ),
+                        ),
+                      },
+                      AspectRatio(
+                        aspectRatio: _aspectRatio,
+                        child: Container(
+                          color: Theme.of(context).cardColor,
+                        ),
+                      ),
+                    ),
+            ),
+          )
+        : widget.gif.images != null &&
+                widget.gif.images.fixedWidth!.webp != null
+            ? true
+                ? CachedNetworkImage(
+                    imageUrl: widget.gif.images.fixedWidth!.webp!,
+                    placeholder: (context, url) => loading(),
+                    errorWidget: (context, url, error) => loading(),
+                    imageBuilder: (context, imageProvider) => Container(
+                          //     width: w,
+                          //   height: w / 1.2,
+                          decoration: BoxDecoration(
+                            //     shape: BoxShape.circle,
+                            //  borderRadius: BorderRadius.all( Radius.circular(10)),
+                            image: DecorationImage(
+                                image: imageProvider, fit: BoxFit.fill),
+                          ),
+                        ))
+                : Image.network(
+                    widget.gif.images.fixedWidth!.webp!,
+                    gaplessPlayback: true,
+                    fit: BoxFit.fill,
+                    loadingBuilder: (context, child, loadingProgress) =>
+                        loading(),
+                    errorBuilder: (context, error, stackTrace) => loading(),
+                  )
+            : loading();
+  }
+
+//////////////////
+  TValue? case2<TOptionType, TValue>(
+    TOptionType selectedOption,
+    Map<TOptionType, TValue> branches, [
+    TValue? defaultValue = null,
+  ]) {
+    if (!branches.containsKey(selectedOption)) {
+      return defaultValue;
+    }
+
+    return branches[selectedOption];
+  }
+
   @override
   Widget build(BuildContext context) => widget.useUrlToSaveMemory
-      ? widget.url != null
-          ? true
-              ? CachedNetworkImage(
-                  imageUrl: widget.url!,
-                  placeholder: (context, url) => loading(),
-                  errorWidget: (context, url, error) => loading(),
-                  imageBuilder: (context, imageProvider) => Container(
-                        //     width: w,
-                        //   height: w / 1.2,
-                        decoration: BoxDecoration(
-                          //     shape: BoxShape.circle,
-                          //  borderRadius: BorderRadius.all( Radius.circular(10)),
-                          image: DecorationImage(
-                              image: imageProvider, fit: BoxFit.fill),
-                        ),
-                      ))
-              : Image.network(
-                  widget.url!,
-                  gaplessPlayback: true,
-                  fit: BoxFit.fill,
-                  loadingBuilder: (context, child, loadingProgress) =>
-                      loading(),
-                  errorBuilder: (context, error, stackTrace) => loading(),
-                )
-          : loading()
+      ? gifWidget()
       : FutureBuilder(
           future: _loadImage,
           builder: (BuildContext context, AsyncSnapshot<Uint8List?> snapshot) {
@@ -140,3 +217,4 @@ class _GiphyRenderImageState extends State<GiphyRenderImage> {
             return loading();
           });
 }
+
